e61252666dbf5d1037706d2917c86e32d5505ba4
Nish Tahir <nishtahir@outlook.com>
2015-10-08 15:54:22 +0300

    Added Simple testing project

---
+++ Module1/IntoToUnitTesting/SimpleTesting/src/ee/ttu/SimpleTesting.java
@@ -0,0 +1,46 @@
+package ee.ttu;
+
+import java.io.File;
+
+/**
+ * Complete the methods and write Unit tests
+ * to make sure that your code works correctly
+ */
+public class SimpleTesting {
+	
+	public SimpleTesting(){
+		
+	}
+
+	public static void main(String[] args) {
+		new SimpleTesting();
+	}
+	
+	/**
+	 * 
+	 * @param value string representation of a number such as "42"
+	 * @return integer value
+	 */
+	public int getIntegerFromString(String value){
+		return 0;
+	}
+	
+	/**
+	 * Read a file and return the content
+	 * 
+	 * @param file
+	 * @return
+	 */
+	public String getContentFromFile(File file){
+		return null;
+	}
+	
+	/**
+	 * Return a number between 1 and 5
+	 * @return
+	 */
+	public int getRandomNumber(){
+		return 0;
+	}
+
+}
--- Module1/ReadMe.md
+++ Module1/ReadMe.md
@@ -1,10 +1,13 @@
 #Exercises
 
-####Debugging
+#Debugging
 Import the project Debugging in Module 1. 
 Use the Debugger built into your IDE to find and fix all the bugs in the code. 
 
-*Tips*
+####Tips
 * Use breakpoints to pause execution when the code reaches a certain statement in your code.
 * Inspect variables to make sure that the code is doing what it's supposed to.
 * Your debugger isn't the only debugging tool you have. Rubber duckys can be super helpful :)
+
+#Unit Testing
+Complete and write tests to cover methods in the SimpleTesting project.
